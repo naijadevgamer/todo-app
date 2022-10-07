58ce7130c26e0b753c4fb24a399017473903088e
thenaijagamer <sabdullahialaba50@gmail.com>
2022-10-07 19:33:47 +0100

    Add the circle hover feature

--- src/components/task-box/task-box.component.jsx
+++ src/components/task-box/task-box.component.jsx
@@ -13,24 +13,6 @@ function TaskBox() {
         <Task />
         <Task />
         <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
       </ul>
       <ul className="task-box__log-box">
         <li className="task-box__log task-box__log--items-left">
--- src/components/task/task.component.jsx
+++ src/components/task/task.component.jsx
@@ -6,6 +6,7 @@ function Task() {
   return (
     <li className="task" draggable={true}>
       <div className="task__circle"></div>
+      <div className="task__circle-hover"></div>
       <div className="task__item">
         Jog around the park 3x
         <div className="task__remove">&#10005;</div>
