b1f050648b2554ef36ae2dc82bc3fe223460e8f9
thenaijagamer <sabdullahialaba50@gmail.com>
2022-10-10 09:55:31 +0100

    fix some bugs

--- src/components/tasks/tasks.component.jsx
+++ src/components/tasks/tasks.component.jsx
@@ -1,31 +1,40 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 import "./tasks.styles.scss";
 
 function Tasks({ tasks, setTask }) {
+  const [check, setCheck] = useState(false);
   const handleDelete = (id) => {
     setTask(tasks.filter((task) => task.id != id));
   };
-  // const handleCompleted = (e) => {
-  //   if (e) return false;
-  //   else return true;
-  // };
-  // console.log(handleCompleted(true));
+  const handle = () => {};
+  useEffect(() => {
+    tasks.map((task) => {
+      // task.completed = !task.completed;
+      // console.log(task);
+      task.addEventListener("change", handle);
+    });
+  }, [check]);
+  const handleChecked = (e) => {
+    if (e.target.checked) {
+      setCheck(true);
+    } else {
+      setCheck(false);
+    }
+  };
   return (
     <ul className="tasks">
       {tasks.map((task) => (
         <li key={task.id} className="task" draggable={true}>
-          <input type="checkbox" className="task__check" id={task.id} />
-          <label
-            className="task__circle"
-            htmlFor={task.id}
-            onClick={() => {
-              if (task.completed == false) task.completed = true;
-              else task.completed = false;
-              console.log(task);
-            }}
-          >
-            {task.completed == false ? "&#10003;" : ""}
+          <input
+            type="checkbox"
+            className="task__check"
+            id={task.id}
+            onChange={handleChecked}
+          />
+          <label className="task__circle" htmlFor={task.id}>
+            {check ? "&#10003;" : ""}
           </label>
           <div className="task__circle-hover"></div>
           <div className="task__item">
