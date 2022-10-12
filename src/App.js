21bf7b89b4a62e8a3761e9c1f9d2e39b4841b7ce
thenaijagamer <sabdullahialaba50@gmail.com>
2022-10-12 17:35:51 +0100

    Add clear completed and active left functionalities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Background from "./components/background/background.component";
 import Header from "./components/header/header.components";
@@ -13,6 +12,22 @@ function App() {
   const [filter, setFilter] = useState("all");
   const [tasks, setTask] = useState([]);
   const [all, setAll] = useState([]);
+  const [countActive, setCountActive] = useState(0);
+
+  useEffect(() => {
+    setCountActive(activeCount());
+  }, [all]);
+
+  function activeCount() {
+    let totalLeft = all.filter((task) => task.completed === false);
+    if (totalLeft.length > 1) {
+      return `${totalLeft.length} items left`;
+    } else if (totalLeft.length === 1) {
+      return `${totalLeft.length} item left`;
+    } else {
+      return "No items left";
+    }
+  }
 
   return (
     <div className="App">
@@ -34,6 +49,7 @@ function App() {
           setAll={setAll}
           filter={filter}
           setFilter={setFilter}
+          countActive={countActive}
         />
         <footer className="footer">Drag and drop to reorder list</footer>
       </div>
--- src/components/task-box/task-box.component.jsx
+++ src/components/task-box/task-box.component.jsx
@@ -4,7 +4,15 @@ import Tasks from "../tasks/tasks.component";
 
 import "./task-box.styles.scss";
 
-function TaskBox({ tasks, setTask, all, setAll, filter, setFilter }) {
+function TaskBox({
+  tasks,
+  setTask,
+  all,
+  setAll,
+  filter,
+  setFilter,
+  countActive,
+}) {
   function handleAll() {
     setTask(all);
     setFilter("all");
@@ -17,40 +25,51 @@ function TaskBox({ tasks, setTask, all, setAll, filter, setFilter }) {
     setTask(all.filter((task) => task.completed === true));
     setFilter("completed");
   }
+  function clearCompleted() {
+    setAll(all.filter((task) => task.completed === false));
+    setTask(all.filter((task) => task.completed === false));
+    setFilter("active");
+  }
+
   return (
     <div className="task-box">
       <Tasks tasks={tasks} setTask={setTask} all={all} setAll={setAll} />
       <ul className="task-box__log-box">
         <li className="task-box__log task-box__log--items-left">
-          5 items left
+          {countActive}
         </li>
         <li className="task-box__filter-box">
-          <span
+          <button
             className={`task-box__filter ${
               filter === "all" ? "task-box__filter--all" : ""
             }`}
             onClick={handleAll}
           >
             All
-          </span>
-          <span
+          </button>
+          <button
             className={`task-box__filter ${
               filter === "active" ? "task-box__filter--active" : ""
             }`}
             onClick={handleActive}
           >
             Active
-          </span>
-          <span
+          </button>
+          <button
             className={`task-box__filter ${
               filter === "completed" ? "task-box__filter--completed" : ""
             }`}
             onClick={handleCompleted}
           >
             Completed
-          </span>
+          </button>
         </li>
-        <li className="task-box__log task-box__log--clear">clear Completed</li>
+        <li
+          className="task-box__log task-box__log--clear"
+          onClick={clearCompleted}
+        >
+          clear Completed
+        </li>
       </ul>
     </div>
   );
